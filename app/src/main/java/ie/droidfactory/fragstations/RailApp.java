f22d7e011b0831870b384278de0a2a017e7ad3ae
Nick Walicki <nwalicki@sensipass.com>
2016-06-13 14:24:09 +0100

    added ACRA crash-report analizer, using remote db ibm-claudant

--- app/src/main/java/ie/droidfactory/fragstations/RailApp.java
+++ app/src/main/java/ie/droidfactory/fragstations/RailApp.java
@@ -4,13 +4,45 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
 /**
  * Created by kudlaty on 12/06/2016.
  */
+
+@ReportsCrashes(
+        formUri = "https://irerail.cloudant.com/acra-irerail/_design/acra-storage/_update/report",
+        reportType = HttpSender.Type.JSON,
+        httpMethod = HttpSender.Method.POST,
+        formUriBasicAuthLogin = "fortyloughtnescordelisai",
+        formUriBasicAuthPassword = "ea3e3b25b79d6baa2f6b3c4dfa0be4d9e9909495",
+//        formKey = "", // This is required for backward compatibility but not used
+        customReportContent = {
+                ReportField.APP_VERSION_CODE,
+                ReportField.APP_VERSION_NAME,
+                ReportField.ANDROID_VERSION,
+                ReportField.PACKAGE_NAME,
+                ReportField.REPORT_ID,
+                ReportField.BUILD,
+                ReportField.STACK_TRACE
+        },
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.toast_crash
+)
 public class RailApp extends Application{
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        ACRA.init(this);
     }
 }
